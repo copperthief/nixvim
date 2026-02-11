297eedd5a071fe44db45b3f04113daf37d8fa4fa
copperthief <copperthiefcorvid@gmail.com>
2026-02-10 22:39:48 -0500

    configured bufferline

--- config/bufferline.nix
+++ config/bufferline.nix
@@ -1,6 +1,16 @@
 {
   plugins = {
-    bufferline.enable = true;
     web-devicons.enable = true;
+    bufferline = {
+      enable = true;
+      settings = {
+        options = {
+          indicator.style = "none";
+          color_icons = false;
+          show_close_icon = false;
+          show_buffer_close_icons = false;
+        };
+      };
+    };
   };
 }
