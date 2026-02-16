fada9565b6fc9cf9d59916eabd233566158627bb
copperthief <copperthiefcorvid@gmail.com>
2026-02-16 14:49:21 -0500

    reverted flake.lock back to when these problems didnt exist

--- config/default.nix
+++ config/default.nix
@@ -23,7 +23,7 @@
     gitsigns.enable = true; # TODO: configure
     todo-comments.enable = true;
     oil.enable = true; # TODO: configure
-    #rustaceanvim.enable = true; # TODO: configure
+    rustaceanvim.enable = true; # TODO: configure
     # TODO: setup dap stuff
     marks.enable = true; # TODO:configure
     markview.enable = true;
@@ -73,7 +73,7 @@
         cmake.enable = true;
         nil_ls.enable = true;
         uiua.enable = true;
-        rust_analyzer.enable = true;
+        #rust_analyzer.enable = true;
         rust_analyzer = {
           installCargo = false;
           installRustc = false;
