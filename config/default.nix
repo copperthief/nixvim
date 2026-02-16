3f84ea9b8648309ff89a9bde92cf907848f60414
copperthief <copperthiefcorvid@gmail.com>
2026-02-16 14:37:13 -0500

    hopefully fixed lsp bug?

--- config/default.nix
+++ config/default.nix
@@ -320,6 +320,12 @@
   extraConfigLuaPost =
     # lua
     ''
+        vim.lsp.handlers["workspace/diagnostic/refresh"] = function(_, _, ctx)
+          local ns = vim.lsp.diagnostic.get_namespace(ctx.client_id)
+          pcall(vim.diagnostic.reset, ns)
+          return true
+        end
+
       require("neorg").setup {
         load = {
           ["core.defaults"] = {},
